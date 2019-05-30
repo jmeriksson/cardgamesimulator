ada645c2e30947ec5ccc66a7fe0eafbbd02ab050
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2019-05-30 17:57:52 +0200

    Initializing a console application.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace me222nm_examination_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
