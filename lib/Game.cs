480b6c1ff4ffb36297f1cf857ea8f34856da1883
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2019-06-10 16:34:01 +0200

    Simplifying console output.

--- lib/Game.cs
+++ lib/Game.cs
@@ -45,9 +45,11 @@ namespace me222nm_examination_3
             Players = players;
         }
 
+        /// <summary>
+        /// Let's each player take turn to play Twenty-One against the dealer. Outputs result in the terminal.
+        /// </summary>
         public void Play ()
         {
-            Console.WriteLine($"Game started. {Dealer.Nickname} hands one card to each player.");
             foreach (Player player in Players)
             {
                 player.hand.Add(Dealer.DrawCard());
@@ -57,110 +59,77 @@ namespace me222nm_examination_3
             {
                 while (player.EvaluateHand() == "hit me" || player.hand.Count < 2)
                 {
-                    Console.WriteLine($"{player.Nickname} has this hand: {player.HandToString()}");
-                    Console.WriteLine($"{player.Nickname}: 'Hit me!'");
-                    Console.WriteLine($"{Dealer.Nickname} hands {player.Nickname} another card.");
                     player.hand.Add(Dealer.DrawCard());
                 }
 
                 switch (player.EvaluateHand())
                 {
                     case "bust":
-                        Console.WriteLine($"{player.Nickname} has this hand: {player.HandToString()}");
-                        Console.WriteLine($"{player.Nickname} is busted.");
-                        foreach (Card card in player.hand)
-                        {
-                            Table.DiscardedCards.Add(card);
-                        }
-                        player.hand.Clear();
+                        Console.WriteLine($"{player.Nickname}: {player.HandToString()}({player.HandValue}) BUSTED!");
+                        Console.WriteLine($"{Dealer.Nickname}: {Dealer.HandToString()}({Dealer.HandValue})");
+                        Console.WriteLine($"{Dealer.Nickname} wins!");
                         Console.WriteLine();
+
                         break;
                     case "twenty-one":
-                        Console.WriteLine($"{player.Nickname} has this hand: {player.HandToString()}");
-                        Console.WriteLine($"{player.Nickname} has Twenty-One and wins against the dealer.");
-                        foreach (Card card in player.hand)
-                        {
-                            Table.DiscardedCards.Add(card);
-                        }
-                        player.hand.Clear();
+                        Console.WriteLine($"{player.Nickname}: {player.HandToString()}({player.HandValue}) TWENTY-ONE!");
+                        Console.WriteLine($"{Dealer.Nickname}: {Dealer.HandToString()}({Dealer.HandValue})");
+                        Console.WriteLine($"{player.Nickname} wins!");
                         Console.WriteLine();
+
                         break;
                     case "hold":
-                        Console.WriteLine($"{player.Nickname} has this hand: {player.HandToString()}");
-                        Console.WriteLine($"{player.Nickname}: 'Hold!'");
                         while (Dealer.EvaluateHand() == "hit me" || Dealer.HandValue <= player.HandValue)
                         {
-                            Console.WriteLine($"{Dealer.Nickname} draws a card.");
                             Dealer.hand.Add(Dealer.DrawCard());
-                            Console.WriteLine($"{Dealer.Nickname} has this hand: {Dealer.HandToString()}");
                         }
                         switch (Dealer.EvaluateHand())
                         {
                             case "bust":
-                                Console.WriteLine($"{Dealer.Nickname} is busted. {player.Nickname} wins.");
+                                Console.WriteLine($"{player.Nickname}: {player.HandToString()}({player.HandValue})");
+                                Console.WriteLine($"{Dealer.Nickname}: {Dealer.HandToString()}({Dealer.HandValue}) BUSTED!");
+                                Console.WriteLine($"{player.Nickname} wins!");
                                 Console.WriteLine();
-                                foreach (Card card in player.hand)
-                                {
-                                    Table.DiscardedCards.Add(card);
-                                }
-                                player.hand.Clear();
-                                foreach (Card card in Dealer.hand)
-                                {
-                                    Table.DiscardedCards.Add(card);
-                                }
-                                Dealer.hand.Clear();
+
                                 break;
                             case "twenty-one":
-                                Console.WriteLine($"{Dealer.Nickname} has Twenty-One and wins against {player.Nickname}.");
+                                Console.WriteLine($"{player.Nickname}: {player.HandToString()}({player.HandValue})");
+                                Console.WriteLine($"{Dealer.Nickname}: {Dealer.HandToString()}({Dealer.HandValue}) TWENTY-ONE!");
+                                Console.WriteLine($"{Dealer.Nickname} wins!");
                                 Console.WriteLine();
-                                foreach (Card card in player.hand)
-                                {
-                                    Table.DiscardedCards.Add(card);
-                                }
-                                player.hand.Clear();
-                                foreach (Card card in Dealer.hand)
-                                {
-                                    Table.DiscardedCards.Add(card);
-                                }
-                                Dealer.hand.Clear();
+
                                 break;
                             case "hold":
                             case "hit me":
+                                Console.WriteLine($"{player.Nickname}: {player.HandToString()}({player.HandValue})");
+                                Console.WriteLine($"{Dealer.Nickname}: {Dealer.HandToString()}({Dealer.HandValue})");
                                 if (Dealer.HandValue >= player.HandValue)
                                 {
-                                    Console.WriteLine($"{Dealer.Nickname} has {Dealer.HandValue} and {player.Nickname} has {player.HandValue}. Dealer wins.");
+                                    Console.WriteLine($"{Dealer.Nickname} wins!");
                                     Console.WriteLine();
-                                    foreach (Card card in player.hand)
-                                    {
-                                        Table.DiscardedCards.Add(card);
-                                    }
-                                    player.hand.Clear();
-                                    foreach (Card card in Dealer.hand)
-                                    {
-                                        Table.DiscardedCards.Add(card);
-                                    }
-                                    Dealer.hand.Clear();
+
                                 }
                                 else
                                 {
-                                    Console.WriteLine($"{Dealer.Nickname} has {Dealer.HandValue} and {player.Nickname} has {player.HandValue}. Player wins.");
+                                    Console.WriteLine($"{player.Nickname} wins!");
                                     Console.WriteLine();
-                                    foreach (Card card in player.hand)
-                                    {
-                                        Table.DiscardedCards.Add(card);
-                                    }
-                                    player.hand.Clear();
-                                    foreach (Card card in Dealer.hand)
-                                    {
-                                        Table.DiscardedCards.Add(card);
-                                    }
-                                    Dealer.hand.Clear();
+
                                 }
                                 break;
                         }
-                        Console.WriteLine();
                         break;
                 }
+
+                foreach (Card card in player.hand)
+                {
+                    Table.DiscardedCards.Add(card);
+                }
+                player.hand.Clear();
+                foreach (Card card in Dealer.hand)
+                {
+                    Table.DiscardedCards.Add(card);
+                }
+                Dealer.hand.Clear();
             }
         }
     }
